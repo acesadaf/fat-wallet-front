89e895104d4b0f7fcc28abc420a5dc3e61e56adc
Sadaf Halim <acesadaf@gmail.com>
2020-06-25 18:37:12 -0500

    added toggle-able graphs

--- src/components/Graphs/Graph.js
+++ src/components/Graphs/Graph.js
@@ -6,38 +6,65 @@ import Pie from "./Pie";
 class Graph extends React.Component {
   constructor(props) {
     super(props);
-    this.choice = "Bar";
-
+    this.state = {
+      choice: "Expenses by Month",
+    };
     this.handleSelect = this.handleSelect.bind(this);
   }
 
-  handleSelect(event) {}
+  handleSelect(eventKey) {
+    this.setState({
+      choice: eventKey,
+    });
+    console.log(eventKey);
+  }
 
   render() {
     return (
-      <Dropdown
-        name="category"
-        value={this.choice}
-        onSelect={this.handleSelect}
-        style={{
-          flex: 10,
-        }}
-      >
-        <Dropdown.Toggle
+      <div>
+        <Dropdown
+          name="category"
+          value={this.choice}
+          onSelect={this.handleSelect}
           style={{
-            width: "100%",
-            color: "black",
-            background: "#fcda4f",
-            border: "#fcda4f",
+            flex: 10,
           }}
-          variant="primary"
-          id="dropdown-basic"
         >
-          {this.selectedCat}
-        </Dropdown.Toggle>
-        <Dropdown.Menu style={{ width: "100%" }}></Dropdown.Menu>
-      </Dropdown>
+          <Dropdown.Toggle
+            style={{
+              width: "100%",
+              color: "black",
+              backgroundColor: "transparent",
+              outline: "none",
+              border: "none",
+              outline: "none !important",
+              boxshadow: "none",
+            }}
+            variant="primary"
+            id="dropdown-basic"
+          >
+            {this.state.choice}
+          </Dropdown.Toggle>
+          <Dropdown.Menu style={{ width: "100%", justifyContent: "center" }}>
+            <Dropdown.Item eventKey="Expenses by Month">
+              Expenses by Month
+            </Dropdown.Item>
+            <Dropdown.Item eventKey="Expenses by Category">
+              Expenses by Category
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+        <ShowGraph choice={this.state.choice}></ShowGraph>
+      </div>
     );
+  }
+}
+
+function ShowGraph({ choice }) {
+  if (choice === "Expenses by Month") {
+    return <Bar />;
+  } else {
+    return <Pie />;
   }
 }
 
